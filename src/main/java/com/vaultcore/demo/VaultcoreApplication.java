e0566f2ee006566cfc6e05933589c5982ca9543d
Srirose <mathibala122005@gmail.com>
2026-01-09 11:36:50 +0530

    Initial Commit - Vaultcore Financial Backend

---
+++ src/main/java/com/vaultcore/demo/VaultcoreApplication.java
@@ -0,0 +1,13 @@
+package com.vaultcore.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultcoreApplication.class, args);
+    }
+}
